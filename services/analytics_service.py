34fe4a3300d3fa8f8261b1cca352dae71c15f590
Uma Hegde <umahegde@Umas-MacBook-Pro.local>
2026-07-04 15:44:42 +0530

    added type-hint

--- services/analytics_service.py
+++ services/analytics_service.py
@@ -6,26 +6,26 @@ employee_service = EmployeeService()
 class DataAnalytics:
 
 
-    def total_employees(self):
+    def total_employees(self)->int:
         employees = employee_service.get_all_employees()
         return len(employees)
 
 
-    def employee_by_department(self, department):
+    def employee_by_department(self, department:str)->list[dict]:
         employees = employee_service.get_all_employees()
         department_employee = [employee for employee in employees if employee["department"] == department]
         return department_employee
 
-    def highest_salary_employee(self):
+    def highest_salary_employee(self)->dict | None:
         employees = employee_service.get_all_employees()
         return max(employees, key=lambda emp: float(emp["salary"]))
 
 
-    def lowest_salary_employee(self):
+    def lowest_salary_employee(self)->dict | None:
         employees = employee_service.get_all_employees()
         return min(employees, key=lambda emp: float(emp["salary"]))
 
-    def average_salary_employee(self):
+    def average_salary_employee(self)->float:
         employees = employee_service.get_all_employees()
         if not employees:
             return 0
@@ -33,12 +33,12 @@ class DataAnalytics:
         return sum(salary)/len(employees)
 
 
-    def employee_above_salary(self, salary):
+    def employee_above_salary(self, salary:str)->list[dict]:
         employees = employee_service.get_all_employees()
         return  [employee for employee in employees if float(employee["salary"]) > float(salary) ]
 
 
-    def group_by_department(self):
+    def group_by_department(self)->dict[str,int]:
         employees = employee_service.get_all_employees()
         department_count = {}
         for employee in employees:
--- utils/validation.py
+++ utils/validation.py
@@ -1,7 +1,7 @@
 from employee_management_system.utils.constants import DEPARTMENTS
 
 class Validation:
-    def validate_emp_id(self, emp_id):
+    def validate_emp_id(self, emp_id:str)->tuple[bool, str]:
         if not emp_id.strip():
           return False, "Employee ID cannot be empty"
 
@@ -10,7 +10,7 @@ class Validation:
 
         return True, ""
 
-    def validate_name(self, name):
+    def validate_name(self, name :str)->tuple[bool, str]:
         if not name.strip():
             return False, "Employee name cannot be empty"
         if len(name) < 3:
@@ -18,21 +18,21 @@ class Validation:
 
         if not name.replace(" ", "").isalpha():
             return False, "Name should contain only alphabets"
-        return True,""
+        return True, ""
 
-    def validate_department(self, department):
+    def validate_department(self, department:str)->tuple[bool, str]:
         department = department.strip()
         if department not in DEPARTMENTS:
          return False, "Invalid Department"
 
         return True, ""
-    def validate_salary(self, salary):
+    def validate_salary(self, salary:str)->tuple[bool, str]:
 
         try:
 
-            salary = float(salary)
+            salary_value = float(salary)
 
-            if salary <= 0:
+            if salary_value <= 0:
                 return False, "Salary should be greater than zero"
 
             return True,""
@@ -41,7 +41,7 @@ class Validation:
 
             return False, "Salary should be numeric"
 
-    def get_valid_input(self, prompt, validation_function):
+    def get_valid_input(self, prompt:str, validation_function)->str:
 
             while True:
 
